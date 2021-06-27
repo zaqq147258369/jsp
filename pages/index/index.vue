ec87c5abfa572c8c4cbfdde969bebd8a3c83fca3
Xuyic <309981749@qq.com>
2021-06-27 22:37:50 +0800

    创建uniapp项目，设置四个主要页面，设置底部导航栏

---
+++ pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="contenting">
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style scoped="scoped">
+	.contenting{
+		margin: 0 15rpx;
+	}
+</style>

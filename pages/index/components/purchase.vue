81c51d5b6406925172046ac43982ced007bd8c47
Xuyic <309981749@qq.com>
2021-06-27 23:10:43 +0800

    首页添加空白子组件

---
+++ pages/index/components/purchase.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		抢购
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>

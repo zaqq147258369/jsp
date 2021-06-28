9fb38c22c0aaaa87455158289e604266a85e5169
xuy <3099817419@qq.com>
2021-06-28 10:08:40 +0800

    Squashed commit of the following:
    commit 81c51d5b6406925172046ac43982ced007bd8c47
    Author: Xuyic <309981749@qq.com>
    Date:   Sun Jun 27 23:10:43 2021 +0800
    
        首页添加空白子组件
    
    commit ec87c5abfa572c8c4cbfdde969bebd8a3c83fca3
    Author: Xuyic <309981749@qq.com>
    Date:   Sun Jun 27 22:37:50 2021 +0800
    
        创建uniapp项目，设置四个主要页面，设置底部导航栏

---
+++ pages/index/components/swiper.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		轮播
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,45 @@
+<template>
+	<view class="contenting">
+		<!-- 搜索 -->
+		<Search></Search>
+		<Swipers></Swipers>
+		<Purchase></Purchase>
+		<List></List>
+		<Commodity></Commodity>
+	</view>
+</template>
+
+<script>
+	import Search from './components/search.vue'
+	import Swipers from './components/swiper.vue'
+	import Purchase from './components/purchase.vue'
+	import List from './components/list.vue'
+	import Commodity from './components/commodity.vue'
+	
+	export default {
+		components:{
+			Search,
+			Swipers,
+			Purchase,
+			List,
+			Commodity,
+		},
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style scoped="scoped">
+	.contenting{
+		margin: 0 15rpx;
+	}
+</style>
